6cf261eb3b22251f4622693132dede451798d37c
MASA-10A\nisitani <masa24104@gmail.com>
2024-06-04 10:44:10 +0900

    [ch03]-[hello-win]を実装

---
+++ ch03/hello-win/src/main.rs
@@ -0,0 +1,84 @@
+use windows_sys::{
+    core::*, 
+    Win32::Foundation::*,
+    Win32::Graphics::Gdi::*,
+    Win32::System::LibraryLoader::GetModuleHandleA, 
+    Win32::UI::WindowsAndMessaging::*,
+    Win32::Media::Audio::*,
+};
+
+fn main() {
+    unsafe {
+        let instance = GetModuleHandleA(std::ptr::null());
+        debug_assert!(instance != 0);
+
+        let app_name = s!("HelloWin");
+
+        let wnd_class = WNDCLASSA {
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(wndproc),
+            cbClsExtra: 0,
+            cbWndExtra: 0,
+            hInstance: instance,
+            hIcon: 0,
+            hCursor: LoadCursorW(0, IDC_ARROW),
+            hbrBackground: GetStockObject(WHITE_BRUSH),
+            lpszMenuName: std::ptr::null(),
+            lpszClassName: app_name,
+        };
+
+        let atom = RegisterClassA(&wnd_class);
+        debug_assert!(atom != 0);
+
+        let hwnd = CreateWindowExA(
+            0,
+            app_name,
+            s!("The Hello Program"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            instance,
+            std::ptr::null(),
+        );
+
+        ShowWindow(hwnd, SW_SHOWNORMAL);
+        UpdateWindow(hwnd);
+
+        let mut message = std::mem::zeroed();
+
+        while GetMessageA(&mut message, 0, 0, 0) != 0 {
+            DispatchMessageA(&message);
+        }
+    }
+}
+
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        match message {
+            WM_CREATE => {
+                PlaySoundA(s!("Windows Background.wav"), 0, SND_FILENAME | SND_ASYNC);
+                0
+            }
+
+            WM_PAINT => {
+                let mut ps: PAINTSTRUCT = std::mem::zeroed();
+                let hdc = BeginPaint(window, &mut ps);
+                let mut rect: RECT = std::mem::zeroed();
+                let _result = GetClientRect(window, &mut rect);
+                DrawTextA(hdc, s!("Hello RUST!"), -1, &mut rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+                EndPaint(window, &ps);
+                0
+            }
+
+            WM_DESTROY => {
+                PostQuitMessage(0);
+                0
+            }
+            _ => DefWindowProcA(window, message, wparam, lparam),
+        }
+    }
+}
