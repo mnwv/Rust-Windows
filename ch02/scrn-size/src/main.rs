541cdd6890e62764bb23139e0c29dca9bf34f4ee
MASA-10A\nisitani <masa24104@gmail.com>
2024-06-04 09:26:21 +0900

    [ch02]-[scrn-size]を実装

---
+++ ch02/scrn-size/src/main.rs
@@ -0,0 +1,37 @@
+use windows_sys::Win32::UI::WindowsAndMessaging::*;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
+macro_rules! to_wide_chars {
+    ($str:expr) => {
+        OsStr::new($str).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
+    }
+}
+
+macro_rules! msgbox_with_format {
+    ($caption:expr, $fmt:expr) => {
+        let caption = to_wide_chars!($caption);
+        let msg = to_wide_chars!($fmt);
+        unsafe {
+            MessageBoxW(0, msg.as_ptr(), caption.as_ptr(), MB_OK)
+        }
+    };
+    ($caption:expr, $fmt:expr, $($arg:tt)*) => {
+        let caption = to_wide_chars!($caption);
+        let formatted_msg = std::fmt::format(format_args!($fmt, $($arg)*));
+        let msg = to_wide_chars!(&formatted_msg);
+        unsafe {
+            MessageBoxW(0, msg.as_ptr(), caption.as_ptr(), MB_OK)
+        }
+   };
+}
+
+fn main() {
+    msgbox_with_format!("ScrnSize", "スクリーン解像度を表示します。");
+    let cx_screen = unsafe { GetSystemMetrics(SM_CXSCREEN) };
+    let cy_screen = unsafe { GetSystemMetrics(SM_CYSCREEN) };
+    msgbox_with_format!("ScrnSize", "The screen is {} pixcels wide by {} pixcels high.",
+                        cx_screen, cy_screen);
+    msgbox_with_format!("スクリーン解像度", "幅: {} ピクセル、高さ: {} ピクセル",
+                        cx_screen, cy_screen);
+}
