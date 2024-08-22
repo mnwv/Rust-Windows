f4f16e39e45072b100b8ceb4ef7d0edad6583040
MASA-10A\nisitani <masa24104@gmail.com>
2024-08-23 08:56:43 +0900

    [ch17]-[ez-font]ez_font.rsの実装終了

--- ch17/ez-font/src/ez_font.rs
+++ ch17/ez-font/src/ez_font.rs
@@ -1,7 +1,15 @@
-use std::arch::x86_64::__cpuid;
-use std::ptr::slice_from_raw_parts;
-use windows::Win32::Foundation::POINT;
-use windows::Win32::Graphics::Gdi::{CreateFontIndirectW, DPtoLP, DeleteObject, GetDeviceCaps, GetTextMetricsW, ModifyWorldTransform, RestoreDC, SaveDC, SelectObject, SetGraphicsMode, SetViewportOrgEx, SetWindowOrgEx, DEFAULT_CHARSET, FONT_CLIP_PRECISION, FONT_OUTPUT_PRECISION, FONT_QUALITY, GM_ADVANCED, HDC, HFONT, HGDIOBJ, HORZRES, HORZSIZE, LOGFONTW, LOGPIXELSX, LOGPIXELSY, MWT_IDENTITY, TEXTMETRICW, VERTRES, VERTSIZE};
+use windows::Win32::{
+    Foundation::POINT,
+    Graphics::Gdi::{
+        CreateFontIndirectW, DPtoLP, DeleteObject, GetDeviceCaps,
+        GetTextMetricsW, ModifyWorldTransform, RestoreDC, SaveDC,
+        SelectObject, SetGraphicsMode, SetViewportOrgEx, SetWindowOrgEx,
+        DEFAULT_CHARSET, GM_ADVANCED, HORZRES, HORZSIZE, LOGPIXELSX,
+        LOGPIXELSY, MWT_IDENTITY, VERTRES, VERTSIZE,
+        FONT_CLIP_PRECISION, FONT_OUTPUT_PRECISION, FONT_QUALITY,
+        HDC, HFONT, LOGFONTW, TEXTMETRICW,
+    }
+};
 
 pub const EZ_ATTR_BOLD: i32 = 1;
 pub const EZ_ATTR_ITALIC: i32 = 2;
@@ -18,9 +26,9 @@ pub fn ez_create_font(
     unsafe {
         SaveDC(hdc);
         SetGraphicsMode(hdc, GM_ADVANCED);
-        ModifyWorldTransform(hdc, None, MWT_IDENTITY);
-        SetViewportOrgEx(hdc, 0, 0, None);
-        SetWindowOrgEx(hdc, 0, 0, None);
+        let _ = ModifyWorldTransform(hdc, None, MWT_IDENTITY);
+        let _ = SetViewportOrgEx(hdc, 0, 0, None);
+        let _ = SetWindowOrgEx(hdc, 0, 0, None);
         let (cx_dpi, cy_dpi) =
         if log_res {
             (GetDeviceCaps(hdc, LOGPIXELSX) as f32,
@@ -33,7 +41,7 @@ pub fn ez_create_font(
             x: deci_pt_width * (cx_dpi / 72.0) as i32,
             y: deci_pt_hight * (cy_dpi / 72.0) as i32,
         }];
-        DPtoLP(hdc, &mut pt);
+        let _ = DPtoLP(hdc, &mut pt);
 
         let mut lf: LOGFONTW = LOGFONTW {
             lfHeight: -(pt[0].y.abs() as f32 / 10.0 + 0.5) as i32,
@@ -51,21 +59,37 @@ pub fn ez_create_font(
             lfClipPrecision: FONT_CLIP_PRECISION(0),
             lfFaceName: {
                 let mut v: Vec<u16> = face_name.encode_utf16().collect();
+                // let mut v = to_wide_chars(face_name);
                 while v.len() < 32 { v.push(0); }
-                v.iter().collect()
+                let slice = v.into_boxed_slice();
+                let result = slice.try_into();
+                if let Some(_) = result.as_ref().err() {
+                    [0; 32]
+                } else {
+                    let boxed_array: Box<[u16; 32]> = result.ok().unwrap();
+                    *boxed_array
+                }
             }
         };
         let mut font = CreateFontIndirectW(&lf);
         if deci_pt_width != 0 {
-            font = SelectObject(hdc, font);
+            let gdiobj = SelectObject(hdc, font);
+            font = HFONT(gdiobj.0);
             let mut tm: TEXTMETRICW = std::mem::zeroed();
-            GetTextMetricsW(hdc, &mut tm);
-            DeleteObject(SelectObject(hdc, font).into());
+            let _ = GetTextMetricsW(hdc, &mut tm);
+            let gdiobj = SelectObject(hdc, font);
+            let _ = DeleteObject(gdiobj);
             lf.lfWidth = tm.tmAveCharWidth * pt[0].x.abs() / pt[0].y.abs(); // +0.5
             font = CreateFontIndirectW(&lf);
         }
-        RestoreDC(hdc, -1);
+        let _ = RestoreDC(hdc, -1);
         font
     }
-    // HFONT(std::ptr::null_mut())
+}
+
+fn to_wide_chars(str: &str) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+
+    OsStr::new(str).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>()
 }
