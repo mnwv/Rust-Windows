22e3a1668d77a2d197452e6bc853af2a0bacfb2c
MASA-10A\nisitani <masa24104@gmail.com>
2024-08-22 23:01:43 +0900

    [ch17]-[ez-font]実装中

--- ch17/ez-font/src/ez_font.rs
+++ ch17/ez-font/src/ez_font.rs
@@ -1,10 +1,12 @@
+use std::arch::x86_64::__cpuid;
+use std::ptr::slice_from_raw_parts;
 use windows::Win32::Foundation::POINT;
-use windows::Win32::Graphics::Gdi::{DPtoLP, GetDeviceCaps, ModifyWorldTransform, SaveDC, SetGraphicsMode, SetViewportOrgEx, SetWindowOrgEx, GM_ADVANCED, HDC, HFONT, HORZRES, HORZSIZE, LOGFONTW, LOGPIXELSX, LOGPIXELSY, MWT_IDENTITY, VERTRES, VERTSIZE};
+use windows::Win32::Graphics::Gdi::{CreateFontIndirectW, DPtoLP, DeleteObject, GetDeviceCaps, GetTextMetricsW, ModifyWorldTransform, RestoreDC, SaveDC, SelectObject, SetGraphicsMode, SetViewportOrgEx, SetWindowOrgEx, DEFAULT_CHARSET, FONT_CLIP_PRECISION, FONT_OUTPUT_PRECISION, FONT_QUALITY, GM_ADVANCED, HDC, HFONT, HGDIOBJ, HORZRES, HORZSIZE, LOGFONTW, LOGPIXELSX, LOGPIXELSY, MWT_IDENTITY, TEXTMETRICW, VERTRES, VERTSIZE};
 
-pub const EZ_ATTR_BOLD: usize = 1;
-pub const EZ_ATTR_ITALIC: usize = 2;
-pub const EZ_ATTR_UNDERLINE: usize = 4;
-pub const EZ_ATTR_STRIKEFONT: usize = 8;
+pub const EZ_ATTR_BOLD: i32 = 1;
+pub const EZ_ATTR_ITALIC: i32 = 2;
+pub const EZ_ATTR_UNDERLINE: i32 = 4;
+pub const EZ_ATTR_STRIKEOUT: i32 = 8;
 
 pub fn ez_create_font(
     hdc: HDC,
@@ -33,9 +35,37 @@ pub fn ez_create_font(
         }];
         DPtoLP(hdc, &mut pt);
 
-        let lf: LOGFONTW = LOGFONTW {
-            lfHeight: fabs
+        let mut lf: LOGFONTW = LOGFONTW {
+            lfHeight: -(pt[0].y.abs() as f32 / 10.0 + 0.5) as i32,
+            lfWidth: 0,
+            lfEscapement: 0,
+            lfOrientation: 0,
+            lfWeight: if attr & EZ_ATTR_BOLD == EZ_ATTR_BOLD { 700 } else { 0 },
+            lfItalic: if attr & EZ_ATTR_ITALIC == EZ_ATTR_ITALIC { 1 } else { 0 },
+            lfUnderline: if attr & EZ_ATTR_UNDERLINE == EZ_ATTR_UNDERLINE { 1 } else { 0 },
+            lfStrikeOut: if attr & EZ_ATTR_STRIKEOUT == EZ_ATTR_STRIKEOUT { 1 } else { 0 },
+            lfCharSet: DEFAULT_CHARSET,
+            lfOutPrecision: FONT_OUTPUT_PRECISION(0),
+            lfQuality: FONT_QUALITY(0),
+            lfPitchAndFamily: 0,
+            lfClipPrecision: FONT_CLIP_PRECISION(0),
+            lfFaceName: {
+                let mut v: Vec<u16> = face_name.encode_utf16().collect();
+                while v.len() < 32 { v.push(0); }
+                v.iter().collect()
+            }
+        };
+        let mut font = CreateFontIndirectW(&lf);
+        if deci_pt_width != 0 {
+            font = SelectObject(hdc, font);
+            let mut tm: TEXTMETRICW = std::mem::zeroed();
+            GetTextMetricsW(hdc, &mut tm);
+            DeleteObject(SelectObject(hdc, font).into());
+            lf.lfWidth = tm.tmAveCharWidth * pt[0].x.abs() / pt[0].y.abs(); // +0.5
+            font = CreateFontIndirectW(&lf);
         }
+        RestoreDC(hdc, -1);
+        font
     }
-    HFONT(std::ptr::null_mut())
+    // HFONT(std::ptr::null_mut())
 }
