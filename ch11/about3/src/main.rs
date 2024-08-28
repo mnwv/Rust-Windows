33b21cd246b27369d9f8ca1a9ce3429821a87ba7
MASA-10A\nisitani <masa24104@gmail.com>
2024-08-28 15:25:38 +0900

    [ch11]-[about3]実装中。ほぼ出来。要整理

--- ch11/about3/src/main.rs
+++ ch11/about3/src/main.rs
@@ -5,10 +5,8 @@ use windows::{
     Win32::Graphics::Gdi::*,
     Win32::System::LibraryLoader::{GetModuleHandleW, },
     Win32::UI::WindowsAndMessaging::*,
-    Win32::UI::Controls::CheckRadioButton,
-    Win32::UI::Input::KeyboardAndMouse::SetFocus,
+    Win32::UI::Input::KeyboardAndMouse::VK_SPACE,
 };
-use windows::Win32::UI::Input::KeyboardAndMouse::GetFocus;
 
 #[macro_use] mod macros;
 // mod resource;
@@ -36,7 +34,7 @@ fn main() -> Result<()> {
         debug_assert!(atom != 0);
 
         wc.style = CS_HREDRAW | CS_VREDRAW;
-        wc.lpfnWndProc = Some(ellips_push_wnd_proc);
+        wc.lpfnWndProc = Some(ellip_push_wnd_proc);
         wc.cbClsExtra = 0;
         wc.cbWndExtra = 0;
         wc.hInstance = instance.into();
@@ -45,8 +43,14 @@ fn main() -> Result<()> {
         // let color_index = COLOR_BTNFACE.0 + 1; // COLOR_BTNFACE + 1 相当
         // let index = color_index.into();
         // wc.hbrBackground = GetSysColorBrush(SYS_COLOR_INDEX(index)); //HBRUSH(COLOR_BTNFACE);
-        wc.hbrBackground = GetSysColorBrush(SYS_COLOR_INDEX(COLOR_BTNFACE.0 + 1)); //HBRUSH(COLOR_BTNFACE);
-        CreateWindowExW(
+        wc.hbrBackground = GetSysColorBrush(SYS_COLOR_INDEX(COLOR_BTNFACE.0)); //HBRUSH(COLOR_BTNFACE);
+        wc.lpszMenuName = PCWSTR::null();
+        wc.lpszClassName = w!("EllipPush");
+
+        let atom = RegisterClassW(&wc);
+        debug_assert!(atom != 0);
+
+        let _hwnd = CreateWindowExW(
             WINDOW_EX_STYLE::default(),
             APP_NAME,
             w!("About Box Demo Program"),
@@ -74,32 +78,27 @@ fn main() -> Result<()> {
 
 extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
+        static mut INSTANCE:HINSTANCE = HINSTANCE(std::ptr::null_mut());
+
         match message {
+            WM_CREATE => {
+                let cs: &CREATESTRUCTW = &*(lparam.0 as *const CREATESTRUCTW);
+                INSTANCE = cs.hInstance;
+                LRESULT(0)
+            }
             WM_COMMAND => {
-                let n = GetWindowLongPtrW(window, GWLP_HINSTANCE);
-                let instance = HINSTANCE(n as *mut core::ffi::c_void);
+                println!("WM_COMMAND wparam.0:{} INSTANCE:{:?}", wparam.0, INSTANCE);
                 match loword!(wparam.0) as u32 {
                     40001 => {
-                        let n = DialogBoxParamW(instance,
-                                                w!("ABOUTBOX"),
-                                                window,
-                                                Some(about_dlg_proc),
-                                                LPARAM(0));
-                        if n != 0 {
-                            InvalidateRect(window, None, TRUE).unwrap();
-                        }
+                        DialogBoxParamW(INSTANCE,
+                                        w!("ABOUTBOX"),
+                                        window,
+                                        Some(about_dlg_proc),
+                                        LPARAM(0));
                         LRESULT(0)
                     },
                     _ => DefWindowProcW(window, message, wparam, lparam),
                 }
-            }
-            WM_PAINT => {
-                let mut ps: PAINTSTRUCT = std::mem::zeroed();
-                BeginPaint(window, &mut ps);
-                EndPaint(window, &mut ps).unwrap();
-
-                paint_window(window, CURRENT_COLOR, CURRENT_FIGURE);
-                LRESULT(0)
             }
             WM_DESTROY => {
                 println!("WM_DESTROY");
@@ -111,75 +110,16 @@ extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: L
     }
 }
 
-extern "system" fn about_dlg_proc(dlg: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> isize {
+extern "system" fn about_dlg_proc(dlg: HWND, message: u32, wparam: WPARAM, _lparam: LPARAM) -> isize {
     unsafe {
-        static mut COLOR: i32 = 0;
-        static mut FIGURE: i32 = 0;
-        static mut CTRL_BLOCK: HWND = HWND(std::ptr::null_mut());
-
         match message {
             WM_INITDIALOG => {
-                println!("WM_INITDIALOG wparam={:?} lparam={:?}", wparam, lparam);
-                let dflt_hwnd = HWND(wparam.0 as *mut core::ffi::c_void);
-                let id = GetDlgCtrlID(dflt_hwnd);   // 1000 IDC_BLACK
-                println!("dflt_hwnd id={}", id);
-                // print_focused_ctrl(dlg);
-
-                COLOR = CURRENT_COLOR;
-                FIGURE = CURRENT_FIGURE;
-                CheckRadioButton(dlg, 1000, 1007, COLOR).unwrap();
-                // IDC_BLACK~IDC_WHITE
-                CheckRadioButton(dlg, 1008, 1009, FIGURE).unwrap();
-                // IDC_RECT~IDC_ELLIPSE
-                let result = GetDlgItem(dlg, 1010 /*IDC_PAINT*/);
-                if let Some(err) = result.as_ref().err() {
-                    panic!("GetDlgItem() failed. Error:{}", err.message());
-                }
-                CTRL_BLOCK = GetDlgItem(dlg, 1010 /*IDC_PAINT*/).unwrap();
-                let result = GetDlgItem(dlg, COLOR);
-                if let Some(err) = result.as_ref().err() {
-                    panic!("GetDlgItem(COLOR) failed. COLOR={} Error:{}", COLOR, err.message());
-                }
-                let color_ctrl = result.ok().unwrap();
-                let result = SetFocus(color_ctrl);
-                if let Some(err) = result.as_ref().err() {
-                    println!("SetFocus(ctrl) failed. ctrl={:?} Error:{}", color_ctrl, err.code());
-                }
-                //
-                // SetFocus(GetDlgItem(dlg, COLOR).unwrap()).unwrap();
-                0
+                TRUE.0 as isize
             }
             WM_COMMAND => {
-                // print_focused_ctrl(dlg);
-                match loword!(wparam.0) as i32 {
-                    1 => {
-                        println!("IDOK");
-                        CURRENT_COLOR = COLOR;
-                        CURRENT_FIGURE = FIGURE;
+                match MESSAGEBOX_RESULT(loword!(wparam.0) as i32) {
+                    IDOK | IDCANCEL => {
                         EndDialog(dlg, TRUE.0 as isize).unwrap();
-                        TRUE.0 as isize
-                    }
-                    2 => {
-                        println!("IDCANCEL");
-                        let _ = EndDialog(dlg, FALSE.0 as isize);
-                        TRUE.0 as isize
-                    }
-                    1000..=1007 => {    // IDC_BLACK ~
-                        COLOR = loword!(wparam.0) as i32;
-                        CheckRadioButton(dlg,
-                                         1000,      // IDC_BLACK
-                                         1007,      // IDC_WHITE
-                                         loword!(wparam.0) as i32).unwrap();
-                        paint_the_block(CTRL_BLOCK, COLOR, FIGURE);
-                        TRUE.0 as isize
-                    }
-                    1008..=1009 => {    // IDC_RECT || IDC_ELLIPSE
-                        FIGURE = loword!(wparam.0) as i32;
-                        CheckRadioButton(dlg,
-                                         1008,      // IDC_RECT
-                                         1009,      // IDC_ELLIPSE
-                                         loword!(wparam.0) as i32).unwrap();
-                        paint_the_block(CTRL_BLOCK, COLOR, FIGURE);
                         TRUE.0 as isize
                     }
                     _ => {
@@ -188,53 +128,77 @@ extern "system" fn about_dlg_proc(dlg: HWND, message: u32, wparam: WPARAM, lpara
                     }
                 }
             }
-            WM_PAINT => {
-                // print_focused_ctrl(dlg);
-                paint_the_block(CTRL_BLOCK, COLOR, FIGURE);
-                FALSE.0 as isize
-            }
             _ => { 0 }
         }
     }
 }
 
-fn paint_the_block(ctrl: HWND, color: i32, figure: i32) {
+extern "system" fn ellip_push_wnd_proc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     unsafe {
-        InvalidateRect(ctrl, None, TRUE).unwrap();
-        UpdateWindow(ctrl).unwrap();
-        paint_window(ctrl, color, figure);
-    }
-}
+        // println!("ellip_push_wnd_proc() message={:?}", message);
+        match message {
+            WM_PAINT => {
+                println!("ellip_push_wnd_proc() WM_PAINT");
+                let mut rc: RECT = RECT::default();
+                let ret = GetClientRect(window, &mut rc);
+                println!("GetClientRect() returns:{:?}", ret);
+                println!("left:{} right:{} top:{} bottom:{}", rc.left, rc.right, rc.top, rc.bottom);
+                let mut text:[u16;40] = [0;40];
+                let ret = GetWindowTextW(window, &mut text);
+                let pcwstr: PCWSTR = PCWSTR::from_raw(text.as_mut_ptr());
+                let len = pcwstr.len();
+                println!("pwstr={:?} len={}", pcwstr, len);
 
-fn paint_window(window: HWND, color: i32, figure: i32) {
-    static COLORS: [COLORREF; 8] = [COLORREF(rgb!(  0,   0,   0)), COLORREF(rgb!(  0,   0, 255)),
-        COLORREF(rgb!(  0, 255,   0)), COLORREF(rgb!(  0, 255, 255)),
-        COLORREF(rgb!(255,   0,   0)), COLORREF(rgb!(255,   0, 255)),
-        COLORREF(rgb!(255, 255,   0)), COLORREF(rgb!(255, 255, 255))];
-    unsafe {
-        let hdc = GetDC(window);
-        let mut rect: RECT = std::mem::zeroed();
-        GetClientRect(window, &mut rect).unwrap();
-        let brush = CreateSolidBrush(COLORS[(color - 1000) as usize]); // IDC_BLACK:1000
-        let brush = HBRUSH(SelectObject(hdc, brush).0);
-        if figure == 1008 {     //IDC_RECT:1008
-            Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom).unwrap();
-        } else {
-            Ellipse(hdc, rect.left, rect.top, rect.right, rect.bottom).unwrap();
+                println!("GetWindowTextW() returns:{:?}", ret);
+                let mut ps= PAINTSTRUCT::default();
+                let hdc = BeginPaint(window, &mut ps);
+                println!("BeginPaint() returns:{:?}", hdc);
+                let brush = CreateSolidBrush(COLORREF(GetSysColor(COLOR_WINDOW)));
+                println!("CreateSolidBrush() returns:{:?}", brush);
+                let brush = HBRUSH(SelectObject(hdc, brush).0);
+                let ret = SetBkColor(hdc, COLORREF(GetSysColor(COLOR_WINDOW)));
+                println!("SetBkColor() returns:{:?}", ret);
+                let ret = SetTextColor(hdc, COLORREF(GetSysColor(COLOR_WINDOWTEXT)));
+                println!("SetTextColor() returns:{:?}", ret);
+                let ret = Ellipse(hdc, rc.left, rc.top, rc.right, rc.bottom);
+                println!("Ellipse() returns:{:?}", ret);
+                // let ret = DrawTextW(hdc, &mut text, &mut rc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+                // let ret = ExtTextOutW(hdc, rc.right, 0, ETO_OPTIONS(0), None, pcwstr, 2, None );
+                // println!("DrawTextW() returns:{:?}", ret);
+                draw_text(window, hdc, pcwstr);
+                let ret = DeleteObject(SelectObject(hdc, brush));
+                println!("DeleteObject() returns:{:?}", ret);
+                let ret = EndPaint(window, &ps);
+                println!("EndPaint() returns:{:?}", ret);
+                LRESULT(0)
+            }
+            WM_KEYUP | WM_LBUTTONUP => {
+                if message == WM_KEYUP && wparam.0 != VK_SPACE.0 as usize {
+                    return DefWindowProcW(window, message, wparam, lparam);
+                }
+                SendMessageW(
+                    GetParent(window).unwrap(),
+                    WM_COMMAND,
+                    WPARAM(GetWindowLongW(window, GWL_ID) as usize),
+                    LPARAM(window.0 as isize));
+                LRESULT(0)
+            }
+            _ => DefWindowProcW(window, message, wparam, lparam)
         }
-        DeleteObject(SelectObject(hdc, brush)).unwrap();
-        ReleaseDC(window, hdc);
     }
 }
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
-unsafe fn print_focused_ctrl(dlg: HWND) {
-    let focus_ctrl = GetFocus();
-    let id = GetDlgCtrlID(focus_ctrl);   // 1000 IDC_BLACK
-    println!("focus_ctrl={:?} id={}", focus_ctrl, id);    // focus_ctrl=HWND(0x0)
+fn draw_text(window: HWND, hdc: HDC, pcwstr: PCWSTR) {
+    use windows_sys::Win32::Foundation::*;
+    use windows_sys::Win32::Graphics::Gdi::*;
+    use windows_sys::Win32::UI::WindowsAndMessaging::*;
+    unsafe {
+        let mut rc: RECT = std::mem::zeroed();
+        GetClientRect(window.0, &mut rc);
+        let hdc = hdc.0;
+        DrawTextW(hdc, pcwstr.0, -1, &mut rc, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+    }
 }
-
 #[allow(dead_code)]
 fn type_of<T>(_: &T) -> &'static str {
     std::any::type_name::<T>()
